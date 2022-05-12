38a461b99228fece98fc772af3384ca561a4d483
osys <lyh9420@icloud.com>
2022-05-12 22:09:57 +0800

    第6章

--- Unit6/src/main/java/LifecycleWebServer.java
+++ Unit6/src/main/java/LifecycleWebServer.java
@@ -2,8 +2,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,13 +13,20 @@ import java.util.logging.Logger;
  * @author osys
  */
 public class LifecycleWebServer {
-    private final ExecutorService exec = Executors.newCachedThreadPool();
+
+    private  int THREAD_NUM = 10;
+    /** 创建执行器 */
+    private final ExecutorService exec = new ThreadPoolExecutor(THREAD_NUM, THREAD_NUM, 0, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(100));
 
     public void start() throws IOException {
+        // 创建 socket 连接
         ServerSocket socket = new ServerSocket(80);
+        // ExecutorService 未关闭
         while (!exec.isShutdown()) {
             try {
+                // 接受Socket连接
                 final Socket conn = socket.accept();
+                // 向 ExecutorService 提交任务
                 exec.execute(new Runnable() {
                     @Override
                     public void run() {
@@ -26,7 +35,7 @@ public class LifecycleWebServer {
                 });
             } catch (RejectedExecutionException e) {
                 if (!exec.isShutdown()) {
-                    log("task submission rejected", e);
+                    log("任务提交被拒绝", e);
                 }
             }
         }
@@ -40,6 +49,7 @@ public class LifecycleWebServer {
         Logger.getAnonymousLogger().log(Level.WARNING, msg, e);
     }
 
+    /** 处理逻辑 */
     void handleRequest(Socket connection) {
         Request req = readRequest(connection);
         if (isShutdownRequest(req)) {
